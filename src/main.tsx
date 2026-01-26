77a3c2e73d0615353eb4dbbc4574aecd4c1f51db
piyushhh11169 <monikakumari54678@gmail.com>
2026-01-26 19:27:49 +0530

    Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
 import AuthPage from './pages/AuthPage';
 import AuthLayout from './Components/AuthLayout';
 import './index.css'
